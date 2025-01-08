4115f70361823ddcf12d732c1f63937a53debe60
Giuseppe Leonardi <leonardigiuseppej@gmail.com>
2025-01-08 17:56:18 +0100

    Delete spotify directory

--- spotify/multivariate.R
+++
@@ -1,8 +0,0 @@
-subset_danceable<- subset(df, dance_f == "danceable")
-subset_more_no<- subset(df, dance_f == "more no than yes")
-subset_more_yes<- subset(df, dance_f == "more yes than no")
-
-
-boxplot(subset_danceable$bpm)
-boxplot(subset_more_no$bpm)
-boxplot(subset_more_yes$bpm)
